11e405efe868a0e4d1e63eb7399eac4470a4a595
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-04-29 18:21:22 +0200

    Add namespace to ThirdPerson scripts. Also add a variable in the controller

--- Assets/Scripts/Player/ThirdPersonCharacterController.cs
+++ Assets/Scripts/Player/ThirdPersonCharacterController.cs
@@ -2,107 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCharacterController : MonoBehaviour
-{
-
-
-    public CharacterAnimator characterAnimator;
-    public CharacterController controller;
-    [Range(0,1)]
-    public float airControl = 0.5f;
+namespace Characters.ThirdPersonCharacter {
     
-    public float Speed = 10f;
-    public float sprint = 1.5f;
-    public float gravity = -20f;
-    public float jumpHeight = 1f;
-    Vector3 velocity;
+    public class ThirdPersonCharacterController : MonoBehaviour {
 
-    //estas variables son pa ver si o personaje ahora mismo esta no suelo
-    public Transform groundCheck;
-    public float groundDistance = 0.4f; //distancia (radio) a que comproba o obxecto
-    public LayerMask groundMask;
-    bool isGrounded;
-
-
-    void PlayerMovement(){
-
-
-        //Collemos o movimiento
-        float hor = Input.GetAxis("Horizontal");
+        public CharacterAnimator characterAnimator;
+        public CharacterController controller;
+        [Range(0,1)]
+        public float airControl = 0.5f;
         
-        //a velocidad de moverse de lado e mais lenta para que sexa coherente
-        float ver = Input.GetAxis("Vertical") * 0.8f;
+        private bool isWalking; // true=> walking; false=>running
+        public float Speed = 10f;
+        public float sprint = 1.5f;
+        public float gravity = -20f;
+        public float jumpHeight = 1f;
+        Vector3 velocity;
 
- 
-
-        Vector3 playerMovement = transform.right * hor + transform.forward * ver; //o movemento do xogador en coordenadas relativas
-    
-    if (isGrounded) { //SOLO NOS MOVEMOS SI ESTAMOS EN EL SUELO
-        if (Input.GetKey(KeyCode.LeftShift)){ //Shft pos sprint
-        
-        
-          controller.Move(playerMovement * Speed * Time.deltaTime * sprint);
-          
-
-        } else {
-            
-          controller.Move(playerMovement * Speed * Time.deltaTime);
+        //estas variables son pa ver si o personaje ahora mismo esta no suelo
+        public Transform groundCheck;
+        public float groundDistance = 0.4f; //distancia (radio) a que comproba o obxecto
+        public LayerMask groundMask;
+        bool isGrounded;
 
 
-            
-
+        // Method to know how much noise is making the player
+        public int GetPlayerStealthProfile() {
+            if (isWalking) {
+                return 0;
+            } else {
+                return 1;
+            }
         }
 
-         if(Input.GetButtonDown("Jump")){
-            velocity.y = Mathf.Sqrt(jumpHeight*-2*gravity); //ecuacion do tiro vertical para o salto
+
+        void PlayerMovement() {
+
+            //Collemos o movimiento
+            float hor = Input.GetAxis("Horizontal");
             
-            characterAnimator.setTriggerJump();
+            //a velocidad de moverse de lado e mais lenta para que sexa coherente
+            float ver = Input.GetAxis("Vertical") * 0.8f;
 
-         }
-    } else {
-     //MOVIMIENTO EN EL AIRE
+            Vector3 playerMovement = transform.right * hor + transform.forward * ver; //o movemento do xogador en coordenadas relativas
+        
+            if (isGrounded) { 
+                //SOLO NOS MOVEMOS SI ESTAMOS EN EL SUELO
+                if (Input.GetKey(KeyCode.LeftShift)){ //Shft pos sprint
+                    isWalking = false;
+                    controller.Move(playerMovement * Speed * Time.deltaTime * sprint);
+                } else {
+                    isWalking = true;
+                    controller.Move(playerMovement * Speed * Time.deltaTime);
+                }
+
+                if(Input.GetButtonDown("Jump")){
+                    velocity.y = Mathf.Sqrt(jumpHeight*-2*gravity); //ecuacion do tiro vertical para o salto
+                    
+                    characterAnimator.setTriggerJump();
+                }
+            } else {
+                //MOVIMIENTO EN EL AIRE   
+                controller.Move(playerMovement * Speed * Time.deltaTime * airControl);
+            }
+
+        }  
+
+        // Update is called once per frame
+        void Update() {
+            //Para saber si estamos no suelo creamos unha mascara debaixo do personaje e miramos si
+            //esa mascara colisiona ca layer ground, si o fai true si non false
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
             
-          controller.Move(playerMovement * Speed * Time.deltaTime * airControl);
-    }
 
+            //poeñmos a variable global ao que estemos nete frame
+            characterAnimator.setGrounded(isGrounded);
 
+            
+            if (isGrounded && velocity.y < 0){ //mira si esta no suelo
+                velocity.y = -2f; //si esta no suelo aplicamos unha forza pa mantelo pegado
+            }
 
-       
+            PlayerMovement(); //chamamos ao movemento
+            
+            //aplicamos a gravedad
+            velocity.y += gravity * Time.deltaTime; 
 
+            characterAnimator.setVerticalSpeed(velocity.y); //para animacions
 
-    }  
-
-    // Update is called once per frame
-
-    void Update()
-    {
-        //Para saber si estamos no suelo creamos unha mascara debaixo do personaje e miramos si
-        //esa mascara colisiona ca layer ground, si o fai true si non false
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        
-
-        //poeñmos a variable global ao que estemos nete frame
-        characterAnimator.setGrounded(isGrounded);
-
-        
-        if (isGrounded && velocity.y < 0){ //mira si esta no suelo
-            velocity.y = -2f; //si esta no suelo aplicamos unha forza pa mantelo pegado
+            // multiplicamos dos veces por deltatime por la ecuacion de la velocidad (1/2g * t^2)
+            
+            controller.Move(velocity * Time.deltaTime);
+            
         }
 
-        PlayerMovement(); //chamamos ao movemento
-        
-        //aplicamos a gravedad
-        velocity.y += gravity * Time.deltaTime; 
-
-        characterAnimator.setVerticalSpeed(velocity.y); //para animacions
-
-        // multiplicamos dos veces por deltatime por la ecuacion de la velocidad (1/2g * t^2)
-        
-        controller.Move(velocity * Time.deltaTime);
-        
+        void Start() {
+            characterAnimator = GetComponent<CharacterAnimator>(); //componente para animacions
+        }
     }
 
-    void Start(){
-        characterAnimator = GetComponent<CharacterAnimator>(); //componente para animacions
-    }
 }
+
