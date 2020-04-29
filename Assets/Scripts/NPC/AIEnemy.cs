e6767f5f9bb1044513b635fd811cedf8ba8b9df8
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-04-29 18:25:15 +0200

    Basic enemy that follow player if it see him

---
+++ Assets/Scripts/NPC/AIEnemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Characters.ThirdPersonCharacter;
+using UnityEngine.AI;
+
+namespace Characters.Enemy {
+
+	public class AIEnemy : MonoBehaviour {
+		
+		public ThirdPersonCharacterController tpcc;
+		// Field of view for the zombie
+		public float fieldOfView = 120f;
+		public float viewDistance = 10f;
+		private bool isAware = false;
+		private NavMeshAgent agent;
+		// Temporary attribute, just for debugging
+		private Renderer renderer;
+
+		public void Start() {
+			agent = GetComponent<NavMeshAgent>();
+			// Just for debugging
+			renderer = GetComponent<Renderer>();
+		}
+
+		public void Update() {
+			if (isAware) {
+				// This function makes the zombie chases the player
+				agent.SetDestination(tpcc.transform.position);
+				// Just for debugging
+				renderer.material.color = Color.red;
+			} else {
+				SearchForPlayer();
+				// Just for debugging
+				renderer.material.color = Color.blue;
+			}
+		}
+		
+		public void SearchForPlayer() {
+			// Check if the player is within the zombie viewing angle
+			if (Vector3.Angle(Vector3.forward, transform.InverseTransformPoint(tpcc.transform.position)) < (fieldOfView/2)){
+				if (Vector3.Distance(tpcc.transform.position, transform.position) < viewDistance) {
+					OnAware();
+				}
+			}
+		}
+
+		public void OnAware() {
+			isAware = true;
+		}
+
+	}
+
+}
