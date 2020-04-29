1a1d80b200db79317815b1b10bebabb6f5190df4
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-04-30 00:07:49 +0200

    Zombie feels when the player is close

--- Assets/Scripts/NPC/PlayerNoise.cs
+++ Assets/Scripts/NPC/PlayerNoise.cs
@@ -1,6 +1,7 @@
 ﻿
 using UnityEngine;
 using Characters.Enemy;
+using Characters.ThirdPersonCharacter;
 
 public class PlayerNoise : MonoBehaviour {
     
@@ -8,21 +9,32 @@ public class PlayerNoise : MonoBehaviour {
     // Distance unit. If the player is closer that this distance, the zombie
     // will hear the sound
     public float ShootSoundIntensity = 20f;
+    public float walkEnemyPerceptionRadius = 1.5f;
+    public float sprintEnemyPerceptionRadius = 4f;
     // For optimization
     public LayerMask zombieLayer;
 
     private AudioSource audioSource;
-
+    private ThirdPersonCharacterController tpcc;
+    private SphereCollider sphereCollider;
 
     public void Start() {
         audioSource = GetComponent<AudioSource>();
+        tpcc = GetComponent<ThirdPersonCharacterController>();
+        sphereCollider = GetComponent<SphereCollider>();
     }
 
     public void Update() {
         if (Input.GetMouseButtonDown(0)){
             // When the user makes a click
-            Debug.Log("Fireee");
             Fire();
+        }
+        if (tpcc.GetPlayerStealthProfile() == 0){
+            // The player is walking
+            sphereCollider.radius = walkEnemyPerceptionRadius;
+        } else {
+            // The player is running
+            sphereCollider.radius = sprintEnemyPerceptionRadius;
         }
     }
 
@@ -37,5 +49,10 @@ public class PlayerNoise : MonoBehaviour {
         }
     }
 
-    
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Enemy")) {
+            other.GetComponent<AIEnemy>().OnAware();
+        }
+    }
+
 }
