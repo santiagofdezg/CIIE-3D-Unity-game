ea5cc359593e23eac0ffba3762e5ba22b46ff717
Alexandre Gosende Monteagudo <a.gosende@udc.es>
2020-05-01 20:24:32 +0200

    creado audio manager e priome a miña branch a melloralo

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+//AudioManager para que sexa mais facil usar audios no xogo
+
+public class AudioManager : MonoBehaviour
+{
+
+//Audios que vai xestionar o manager
+
+    
+    public Sound[] sounds;
+
+    //Singleton para limitar el numero de AudioManagers a 1
+    public static AudioManager instance;
+
+void createAudioSource(){
+    //A todos os audios no manager, añadimoslle un audio source no propio obxecto
+    //A cada sound añadimoslle unha referencia a ese audio source
+    foreach (Sound s in sounds){
+        //añadimoslle ao audiosource os parametros para poder modificalos dende o inspector
+        s.source = gameObject.AddComponent<AudioSource>();
+        s.source.clip = s.clip;
+        s.source.volume = s.volume;
+        s.source.pitch = s.pitch;
+    }
+
+}
+
+//chamase antes do Start(), asegura que os Audios esten preparados
+    void Awake() 
+    {
+        //Singleton
+        if (instance == null)
+            instance = this;
+        else{
+            Destroy(gameObject);
+        }
+
+        //evitar a volver crear o obxecto nos cambios de escena
+        DontDestroyOnLoad(gameObject);
+
+        createAudioSource();
+
+        
+    }
+
+    void Start(){
+        Play("Theme");
+    }
+
+
+
+//reproducimos o sonido que queiramos
+    public void Play (string name){
+        //buscamos o audio no array
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s==null){
+            Debug.LogWarning("Audio: "+name+" non existe!");
+            return;
+        }
+
+        //reproducimolo
+        s.source.Play();
+    }
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+
+//Esta clase e unha representacion de Sound para gardar a Info
+//Usada co AudioManager fai que sexa mais visible utilizar sons no xogo
+//Sin ter que instancialo usando objetos e prefabs
+
+
+[System.Serializable] //para que aparezca no inspector de unity
+public class Sound 
+{
+//Fonte de audio de cada clase Sound
+    public AudioClip clip;
+
+
+//Si fora necesario podense añadir mais parametros
+    public string name;
+    [Range(0f,1f)]
+    public float volume =0;
+    [Range(.3f,3f)]
+    public float pitch = 1;
+    public bool loop;
+    //parent object of the sound
+    public GameObject parent;
+
+    [HideInInspector] //ocultamolo no inspector para que non se poida modificar dese ahi
+    //pero e publico para que se poida modificar via script
+    public AudioSource source;
+
+}
