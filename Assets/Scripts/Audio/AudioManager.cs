f54355dc70839e5fc0dc1ffbc6047b9ca954545f
Alexandre Gosende Monteagudo <a.gosende@udc.es>
2020-05-01 22:02:54 +0200

    mellorado o audiomanager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,6 +4,9 @@ using UnityEngine.Audio;
 
 //AudioManager para que sexa mais facil usar audios no xogo
 
+//Este xestor permite que cada clip agregado ao sound manager, poida ser iniciado e parado
+//Mesmos son para diferentes obxectos obligan a aumentar o array de sounds
+
 public class AudioManager : MonoBehaviour
 {
 
@@ -20,10 +23,11 @@ void createAudioSource(){
     //A cada sound añadimoslle unha referencia a ese audio source
     foreach (Sound s in sounds){
         //añadimoslle ao audiosource os parametros para poder modificalos dende o inspector
-        s.source = gameObject.AddComponent<AudioSource>();
-        s.source.clip = s.clip;
-        s.source.volume = s.volume;
-        s.source.pitch = s.pitch;
+
+    //si o son ten obxecto parent añadimolo como child ali, si non como child do propio audiomanager
+       s.updateVariables(gameObject);
+
+        
     }
 
 }
@@ -47,15 +51,21 @@ void createAudioSource(){
     }
 
     void Start(){
-        Play("Theme");
+
+        Play("Test");
+
     }
 
 
+    private Sound searchSound(string name){
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        return s;
+    }
 
 //reproducimos o sonido que queiramos
     public void Play (string name){
         //buscamos o audio no array
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Sound s = searchSound(name);
 
         if (s==null){
             Debug.LogWarning("Audio: "+name+" non existe!");
@@ -65,4 +75,44 @@ void createAudioSource(){
         //reproducimolo
         s.source.Play();
     }
+
+    public void Stop (string name){
+        Sound s = searchSound(name);
+
+        if (s==null){
+            Debug.LogWarning("Audio: "+name+" non existe!");
+            return;
+        }
+
+        s.source.Stop();
+
+    }
+
+    public void UnPause(string name){
+        Sound s = searchSound(name);
+
+        if (s==null){
+            Debug.LogWarning("Audio: "+name+" non existe!");
+            return;
+        }
+
+        s.source.Pause();
+
+
+    }
+
+    public void Pause(string name){
+
+        Sound s = searchSound(name);
+
+        if (s==null){
+            Debug.LogWarning("Audio: "+name+" non existe!");
+            return;
+        }
+
+        s.source.UnPause();
+
+
+    }
+
 }
--- Assets/Scripts/Audio/Sound.cs
+++ Assets/Scripts/Audio/Sound.cs
@@ -6,6 +6,8 @@ using UnityEngine.Audio;
 //Usada co AudioManager fai que sexa mais visible utilizar sons no xogo
 //Sin ter que instancialo usando objetos e prefabs
 
+//Solo garda os parametros que nos interesa modificar 
+
 
 [System.Serializable] //para que aparezca no inspector de unity
 public class Sound 
@@ -13,19 +15,40 @@ public class Sound
 //Fonte de audio de cada clase Sound
     public AudioClip clip;
 
+    public GameObject parent;
 
 //Si fora necesario podense añadir mais parametros
     public string name;
     [Range(0f,1f)]
     public float volume =0;
     [Range(.3f,3f)]
-    public float pitch = 1;
+    public float pitch = 1f;
+   [Range(0f,1f)]
+    public float spatialBlend = 0f;
+
     public bool loop;
     //parent object of the sound
-    public GameObject parent;
+    
 
     [HideInInspector] //ocultamolo no inspector para que non se poida modificar dese ahi
     //pero e publico para que se poida modificar via script
     public AudioSource source;
+
+//esta funcion actualiza os parametros de objeto e crea un audio source
+    public void updateVariables(GameObject g){
+
+         if (parent == null){
+            parent = g;
+        }
+        
+        source = parent.AddComponent<AudioSource>();
+        source.spatialBlend = this.spatialBlend;
+        source.clip = this.clip;
+        source.volume = this.volume;
+        source.pitch = this.pitch;
+       
+
+    }
+
 
 }
