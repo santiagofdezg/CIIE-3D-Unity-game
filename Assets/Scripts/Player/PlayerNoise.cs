80b310b87feb3f15ee8b44f09b36b049be4b4252
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-05-03 23:32:30 +0200

    Add sound when shooting

--- Assets/Scripts/Player/PlayerNoise.cs
+++ Assets/Scripts/Player/PlayerNoise.cs
@@ -5,30 +5,26 @@ using Characters.ThirdPersonCharacter;
 
 public class PlayerNoise : MonoBehaviour {
     
-    public AudioClip shootSound;
-    // Distance unit. If the player is closer that this distance, the zombie
-    // will hear the sound
-    public float ShootSoundIntensity = 20f;
+    // public AudioClip shootSound;
+    
+    // public float ShootSoundIntensity = 20f;
+    // public AudioManager audioManager;
     public float walkEnemyPerceptionRadius = 2f;
     public float sprintEnemyPerceptionRadius = 4f;
     // For optimization
     public LayerMask zombieLayer;
 
-    private AudioSource audioSource;
+    // private AudioSource audioSource;
     private ThirdPersonCharacterController tpcc;
     private SphereCollider sphereCollider;
+    // private Weapon weapon;
 
     public void Start() {
-        audioSource = GetComponent<AudioSource>();
         tpcc = GetComponent<ThirdPersonCharacterController>();
         sphereCollider = GetComponent<SphereCollider>();
     }
 
     public void Update() {
-        if (Input.GetMouseButtonDown(0)){
-            // When the user makes a click
-            Fire();
-        }
         if (tpcc.GetPlayerStealthProfile() == 0){
             // The player is walking
             sphereCollider.radius = walkEnemyPerceptionRadius;
@@ -38,17 +34,19 @@ public class PlayerNoise : MonoBehaviour {
         }
     }
 
-    // PLACE HERE THE LOGIC OF THE SHOOTING
-    public void Fire(){
-        audioSource.PlayOneShot(shootSound);
+    // This function is called from the Weapon class and checks if the enemy
+    // heard the shot
+    public void isEnemyHearingShoot(float ShotSoundIntensity){
         // Array of zombies that are within the overlapped sphere of the player
         // OverlapSphere() is very compute-intensive, don't run it in Update()
-        Collider[] zombies = Physics.OverlapSphere(transform.position, ShootSoundIntensity, zombieLayer);
+        Collider[] zombies = Physics.OverlapSphere(transform.position, ShotSoundIntensity, zombieLayer); // ShotSoundIntensity is a distance unit. If the player is closer that this distance, the zombie will hear the sound
         for (int i = 0; i < zombies.Length; i++) {
             zombies[i].GetComponent<AIEnemy>().OnAware();
         }
     }
 
+    // Method that check if the enemy is hearing the player. If it's true then
+    // change the enemy state to "aware"
     public void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Enemy")) {
             other.GetComponent<AIEnemy>().OnAware();
--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -7,10 +7,19 @@ public class Weapon : MonoBehaviour
     public float range = 100f;
     public Camera cam;
     public ParticleSystem flash;
+
+    // Manage sounds
+    public AudioManager audioManager;
+    private PlayerNoise playerNoiseManager;
+    // ShotSoundIntensity is a distance unit. If the player is closer that this 
+    // distance, the zombie will hear the sound. It can be different for each
+    // type of weapon.
+    public float shotSoundIntensity = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerNoiseManager = GetComponentInParent<PlayerNoise>();
     }
 
     // Update is called once per frame
@@ -25,6 +34,8 @@ public class Weapon : MonoBehaviour
     void Shoot(){
         RaycastHit hit;
         flash.Play();
+        audioManager.Play("Shot", gameObject);
+        playerNoiseManager.isEnemyHearingShoot(shotSoundIntensity); 
         if (Physics.Raycast(cam.transform.position , cam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
