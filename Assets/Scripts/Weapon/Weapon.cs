de3a7231d65292b7e0f246da1a716a52d2f51885
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-05-04 01:26:00 +0200

    Weapon script to Player. Update player prefab

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -21,7 +21,7 @@ public class Weapon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerNoiseManager = GetComponentInParent<PlayerNoise>();
+        playerNoiseManager = GetComponent<PlayerNoise>();
     }
 
     // Update is called once per frame
