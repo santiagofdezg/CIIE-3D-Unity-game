2859810d47725518781bd07eaa9ae0d74b81f299
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-05-12 21:29:16 +0200

    New buttons for weapon switching

--- Assets/Scripts/Weapon/WeaponSwitching.cs
+++ Assets/Scripts/Weapon/WeaponSwitching.cs
@@ -12,54 +12,27 @@ public class WeaponSwitching : MonoBehaviour {
 
 
     void Update() {
-     //   int previousSelectedWeapon = selectedWeapon;
-       // if (Input.GetAxis("Mouse ScrollWheel") > 0f){
-         //   Debug.Log("ola");
-           // // After reaching the last weapon, return to the first one
-            //selectedWeapon = selectedWeapon++ % transform.childCount;
-        //} else if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
-          //  Debug.Log("adios");
-            // After reaching the first weapon, return to the last one
-            //selectedWeapon = selectedWeapon-- % transform.childCount;
-        //} 
         int previousSelectedWeapon = selectedWeapon;
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
-        {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f || Input.GetButtonDown("Weapon Next")) {
             if (selectedWeapon >= transform.childCount -1)
+                // After reaching the last weapon, return to the first one
                 selectedWeapon = 0;
             else 
                 selectedWeapon++;
         }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
-        {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f || Input.GetButtonDown("Weapon Previous")) {
             if(selectedWeapon <= 0)
+                // After reaching the first weapon, return to the last one
                 selectedWeapon = transform.childCount - 1;
             else
                 selectedWeapon--;
         }
 
-        if (previousSelectedWeapon != selectedWeapon) 
-        {
+        if (previousSelectedWeapon != selectedWeapon) {
             SelectWeapon();
         }
-    
 
-        
-        // FAI O MESMO PERO CON MÁIS CÓDIGO
-        // if (Input.GetAxis("Mouse ScrollWheel") > 0f){
-        //     // After reaching the last weapon, return to the first one
-        //     if (selectedWeapon >= transform.childCount -1)
-        //         selectedWeapon = 0;
-        //     else
-        //         selectedWeapon++;
-        // } else if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
-        //     // After reaching the first weapon, return to the last one
-        //     if (selectedWeapon <= 0)
-        //         selectedWeapon = transform.childCount - 1;
-        //     else
-        //         selectedWeapon--;
-        // }
     }
 
     void SelectWeapon() {
