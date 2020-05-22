dc54074cba01771e0a01cd84cb19ac74cc70ca92
antonrear <anton116@hotmail.es>
2020-05-22 21:45:38 +0200

    `pruebas dialogo(non funciona)

---
+++ Assets/Scripts/UI/HUD/Dialogue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    // Start is called before the first frame update
+   
+    public string name;
+
+    [TextArea(1,10)]
+    public string[] senteces;
+
+    
+}
