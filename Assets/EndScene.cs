3df21e768ce65eb73892d7514284d66f3ce0db27
Alexandre Gosende Monteagudo <a.gosende@udc.es>
2020-05-23 20:42:39 +0200

    creado escene fin

---
+++ Assets/EndScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.Play("EndTheme", false, 0);
+    }
+
+
+    public void OnExitClick(){
+        Application.Quit();
+    }
+}
