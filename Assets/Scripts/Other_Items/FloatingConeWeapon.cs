583882193604391e22e254337c04f2980a0e0548
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-05-24 01:56:07 +0200

    Create cones for objects that can be picked up

---
+++ Assets/Scripts/Other_Items/FloatingConeWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingConeWeapon : MonoBehaviour {
+
+    public Weapon objectToPick;
+
+
+    void Update() {
+        if (objectToPick == null || !objectToPick.gameObject.activeInHierarchy){
+            gameObject.SetActive(false);
+        }
+    }
+}
