0b28b1b6568bd70604531f4a83ab202c8df15286
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-05-25 13:22:05 +0200

    Final note

---
+++ Assets/Scripts/Other_Items/FinalNote/ActivateNote.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateNote : MonoBehaviour {
+    
+    void OnDestroy() {
+        GameHandler.instance.onBossDied -= ActivateObject;
+
+    }
+    void Start() {
+        gameObject.SetActive(false);
+        GameHandler.instance.onBossDied += ActivateObject;
+    }
+
+    void ActivateObject() {
+        gameObject.SetActive(true);
+    } 
+}
